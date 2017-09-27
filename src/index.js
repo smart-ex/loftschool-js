1ca24f6725abd33be8c003e87a18cb4424559e52
smart <smart@localhost.localdomain>
2017-09-27 15:00:35 +1000

    Почти заверстал, добавил DnD

--- src/index.js
+++ src/index.js
@@ -1,43 +1,38 @@
-/* ДЗ 7.1 - BOM */
+import style from "./style.css";
 
-/**
- * Функция должна создавать окно с указанным именем и размерами
- *
- * @param {number} name - имя окна
- * @param {number} width - ширина окна
- * @param {number} height - высота окна
- * @return {Window}
- */
-function createWindow(name, width, height) {
-    return window.open("about:blank", name, `width=${width},height=${height}`);
+var cont = document.querySelector("#homework-container");
+var list = document.querySelectorAll(".list");
+function handleDrag(e) {
+    e.preventDefault();
 }
 
-/**
- * Функция должна закрывать указанное окно
- *
- * @param {Window} window - окно, размер которого надо изменить
- */
-function closeWindow(window) {
-    window.close();
+cont.ondragenter = handleDrag;
+cont.ondragover = handleDrag;
+
+cont.ondragstart = function(e) {
+    document
+        .querySelectorAll(".dragged")
+        .forEach(el => el.classList.remove("dragged"));
+    if (e.target.className == "item") {
+        // draggedID = e.target.id;
+        // e.dataTransfer.effectAllowed = "move";
+        e.target.classList.add("dragged");
+    } else {
+        e.preventDefault();
+    }
+};
+
+function drop(e) {
+    e.preventDefault();
+
+    let item = document.querySelector(".dragged");
+
+    // console.log(this);
+    if (this.classList.contains("list")) {
+        this.appendChild(item);
+    }
+    item.classList.remove("dragged");
 }
 
-/**
- * Функция должна создавать cookie с указанными именем и значением
- *
- * @param name - имя
- * @param value - значение
- */
-function createCookie(name, value) {
-    document.cookie = `${name}=${value}`;
-}
-
-/**
- * Функция должна удалять cookie с указанным именем
- *
- * @param name - имя
- */
-function deleteCookie(name) {
-    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
-
-export { createWindow, closeWindow, createCookie, deleteCookie };
+list.forEach(el => el.addEventListener("drop", drop, false));
+// cont.addEventListener("drop", drop, false);
